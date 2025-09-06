f3b2331fce949836281f85dea12218d1ac77c33a
nischay gadher <nischaygadher15@gmail.com>
2025-09-06 14:47:10 +0530

    Client url updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todomasterfordev.netlify.app",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
